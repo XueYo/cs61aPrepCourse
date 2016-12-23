d13d6c5b12e6c885f4ec1b56febf0b1882856bcb
XueYo <snow4eternity@gmail.com>
2016-12-23 13:32:49 -0800

    first hw

---
+++ Chapter 2/calc.rb
@@ -0,0 +1,15 @@
+puts 1+2
+puts (4.0+10.5) * 8
+puts 11*3.4+2.6
+puts 12/ 12.3 + 92
+puts 4.6-(7.8-11.5)*8
+puts "hours in a year"
+puts 365*24
+puts "minutes in a decade"
+puts 365*10*24*60
+puts "My age in seconds"
+puts (365*18+97)*24*60*60 + 22*60*60
+puts " My age"
+puts 576108000 /60/60/24/365
+puts "the author's age"
+puts (1160*1000000) / 60 /60 /24 /365
