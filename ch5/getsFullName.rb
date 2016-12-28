e1e4a3970c554e739eedfdf8feaeaa6746eda00c
XueYo <snow4eternity@gmail.com>
2016-12-28 14:56:48 -0800

    Format change

---
+++ ch5/getsFullName.rb
@@ -0,0 +1,7 @@
+puts 'What is your last name?'
+lname = gets.chomp
+puts 'What is your first name?'
+fname = gets.chomp
+puts 'What is your middle name?'
+mname = gets.chomp
+puts 'Hi '+ fname+' '+mname+' '+lname+'! I never heard of this name before!'
