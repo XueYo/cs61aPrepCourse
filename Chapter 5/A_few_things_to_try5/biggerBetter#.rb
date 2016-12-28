3b43e6630a86c5ab65a5ff7bd31ccaf3d1f3b7a6
XueYong Liu <snow4eternity@gmail.com>
2016-12-28 15:27:48 -0800

    Format change

--- Chapter 5/A_few_things_to_try5/biggerBetter#.rb
+++
@@ -1,4 +0,0 @@
-puts 'What is your favorite number in the whole wide world?'
-favnum = gets.chomp.to_i
-biggernum = favnum+1
-puts 'I know a number that surpasses your number. It is '+biggernum.to_s+'. This number holds a special place in my heart. <3'
